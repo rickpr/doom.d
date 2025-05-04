df9ba688c1e4b3dfdd7cdd4779d791afa564b83f
Ricardo Piro-Rael <fdisk@fdisk.co>
2025-05-04 15:02:34 -0700

    Initial commit

---
+++ custom.el
@@ -0,0 +1,19 @@
+;;; custom.el --- Needed to run after 29.1 update    -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  Ricardo Piro-Rael
+
+;; Author: Ricardo Piro-Rael <fdisk87@yahoo.com>
+;; Keywords: 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(js-indent-level 2)
+ '(package-selected-packages '(compat)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
